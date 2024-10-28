864bbfa803e4940fd328b24cec260c5892477660
Nguyen Minh Tuan <tnowad@gmail.com>
2024-10-28 17:12:06 +0700

    feat: add store token to cookie

--- client/src/hooks/api/sign-in.mutation.ts
+++ client/src/hooks/api/sign-in.mutation.ts
@@ -1,3 +1,5 @@
+import { setCookie } from "cookies-next";
+
 import { signInApi } from "@/lib/apis/sign-in.api";
 import {
   SignInErrorResponseSchema,
@@ -5,8 +7,11 @@ import {
   SignInResponseSchema,
 } from "@/lib/schemas/sign-in.schema";
 import { useMutation } from "@tanstack/react-query";
+import { getQueryClient } from "@/app/get-query-client";
 
 export function useSignInMutation() {
+  const queryClient = getQueryClient();
+
   return useMutation<
     SignInResponseSchema,
     SignInErrorResponseSchema,
@@ -15,7 +20,11 @@ export function useSignInMutation() {
     mutationKey: ["sign-in"],
     mutationFn: (body) => signInApi(body),
     onSuccess: (data) => {
-      console.log(data);
+      setCookie("access_token", data.tokens.accessToken);
+      setCookie("refresh_token", data.tokens.refreshToken);
+      queryClient.invalidateQueries({
+        queryKey: ["me"],
+      });
     },
   });
 }
