50d4233a726e0eda5b2bbfe0bc742c5a2f974a30
Nguyen Minh Tuan <tnowad@gmail.com>
2024-11-06 20:19:16 +0700

    feat: add refresh token api

--- client/src/lib/api/client.ts
+++ client/src/lib/api/client.ts
@@ -1,4 +1,9 @@
+import { COOKIE_KEY_ACCESS_TOKEN, COOKIE_KEY_REFRESH_TOKEN } from "@/constants";
 import axios, { AxiosRequestConfig, AxiosResponse, isAxiosError } from "axios";
+import { getCookie, setCookie } from "cookies-next";
+import { jwtDecode } from "jwt-decode";
+import { accessTokenPayloadSchema } from "../schemas/token.schema";
+import { refreshTokenApi } from "../apis/refresh-token.api";
 
 const client = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
@@ -16,12 +21,97 @@ export const apiClient = {
       config,
     );
   },
+
+  get: <TResponse = unknown, TQueryParams = unknown>(
+    url: string,
+    params?: TQueryParams,
+    config?: AxiosRequestConfig<TQueryParams>,
+  ): Promise<AxiosResponse<TResponse, TQueryParams>> => {
+    return client.get<TResponse, AxiosResponse<TResponse, TQueryParams>>(url, {
+      params,
+      ...config,
+    });
+  },
+
+  put: <TResponse = unknown, RRequest = unknown>(
+    url: string,
+    data: RRequest,
+    config?: AxiosRequestConfig<RRequest>,
+  ): Promise<AxiosResponse<TResponse, RRequest>> => {
+    return client.put<TResponse, AxiosResponse<TResponse>, RRequest>(
+      url,
+      data,
+      config,
+    );
+  },
 };
 
-client.interceptors.response.use(
-  (response) => {
-    return response;
+let isRefreshing = false;
+let refreshPromise: Promise<string> | null = null;
+
+async function getAccessToken(): Promise<string | null> {
+  let accessToken: string | undefined = undefined;
+
+  try {
+    accessToken = getCookie(COOKIE_KEY_ACCESS_TOKEN);
+    if (accessToken) {
+      const payload = accessTokenPayloadSchema.parse(jwtDecode(accessToken));
+      if (payload.exp * 1000 > Date.now()) {
+        return accessToken;
+      }
+    }
+  } catch (error) {
+    console.error("Failed to parse access token payload", error);
+  }
+
+  let refreshToken: string | undefined = undefined;
+
+  try {
+    refreshToken = getCookie(COOKIE_KEY_REFRESH_TOKEN);
+    if (!refreshToken) {
+      return null;
+    }
+  } catch (error) {
+    console.error("Failed to get refresh token", error);
+    return null;
+  }
+
+  if (!isRefreshing) {
+    isRefreshing = true;
+    refreshPromise = refreshTokenApi({ refreshToken })
+      .then((response) => {
+        const newAccessToken = response.accessToken;
+        setCookie(COOKIE_KEY_ACCESS_TOKEN, newAccessToken);
+        return newAccessToken;
+      })
+      .catch((error) => {
+        throw error;
+      })
+      .finally(() => {
+        isRefreshing = false;
+        refreshPromise = null;
+      });
+  }
+
+  return refreshPromise;
+}
+
+client.interceptors.request.use(
+  async (config) => {
+    const accessToken = await getAccessToken();
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
   },
+  null,
+  {
+    runWhen: (request) => !request.headers["No-Auth"],
+  },
+);
+
+client.interceptors.response.use(
+  (response) => response,
   (error) => {
     if (isAxiosError(error)) {
       if (error.code === "ERR_NETWORK") {
@@ -38,3 +128,5 @@ client.interceptors.response.use(
     };
   },
 );
+
+export default client;
