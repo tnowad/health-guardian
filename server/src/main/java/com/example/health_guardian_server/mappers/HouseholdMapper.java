eeba0faa65ed6736bd46b7aa1fa6ca1253cb3b7c
Đặng Tuấn Kiệt <kietdang1608@gmail.com>
2024-12-05 01:31:09 +0700

    update mapping of household

--- server/src/main/java/com/example/health_guardian_server/mappers/HouseholdMapper.java
+++ server/src/main/java/com/example/health_guardian_server/mappers/HouseholdMapper.java
@@ -4,9 +4,11 @@ import com.example.health_guardian_server.dtos.requests.CreateHouseholdRequest;
 import com.example.health_guardian_server.dtos.responses.HouseholdResponse;
 import com.example.health_guardian_server.entities.Household;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface HouseholdMapper {
+  @Mapping(source = "patient.id", target = "patientId")
 
   HouseholdResponse toHouseholdResponse(Household household);
 
