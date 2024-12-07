4465f6c79eb7169a4a786dadd21eb43a9d958266
TanDat6189 <116256059+TanDat6189@users.noreply.github.com>
2024-12-07 21:58:51 +0700

    remove unuse import

--- client/src/app/(main)/appointment-management/page.tsx
+++ client/src/app/(main)/appointment-management/page.tsx
@@ -10,7 +10,6 @@ import {
   DialogContent,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from "@/components/ui/dialog";
 import {
   Select,
@@ -19,7 +18,6 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
-import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import {
